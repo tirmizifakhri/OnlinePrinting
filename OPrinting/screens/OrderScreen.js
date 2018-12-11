9e56a35a30e2a01297743458183eb0050c772129
Fulanah <alarva36@yahoo.com>
2018-12-11 21:20:43 +0800

    Update OrderScreen.js

--- OPrinting/screens/OrderScreen.js
+++ OPrinting/screens/OrderScreen.js
@@ -1,31 +1,23 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { Component } from 'react';
+//import { View, Text, StyleSheet, Button } from "react-native";
+import { render } from 'react-dom';
+import '../App.css';
+import FormContainer from '../containers/FormContainer';
 
-export default class OrderScreen extends Component {
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+class OrderScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-        <Text>Order</Text>
-        </View>
-        <View style={styles.button}>
-        <Button onPress={() => this.props.navigation.navigate('Payment')} title="Pay Now" />
-        </View>
-      </View>
+      <div className="col-md-6">
+        <h3> Sample Form Container </h3>
+        <FormContainer />
+      </div>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    textAlignVertical: 'top',
-  },
-  button: {
-    flexDirection: 'row'  
-  }
-});
+render(<App />, document.getElementById("root"));
