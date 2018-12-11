5a425c5bcf1bf04789159af1327aede761b8174e
Fulanah <alarva36@yahoo.com>
2018-12-11 22:43:26 +0800

    Update FormContainer.jsx

--- OPrinting/containers/FormContainer.jsx
+++ OPrinting/containers/FormContainer.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';  
 
 /* Import Components */
-import CheckBox from '../components/CheckBox';  
-import Input from '../components/Input';  
-import TextArea from '../components/TextArea';  
-import Select from '../components/Select';
-import Button from '../components/Button'
+import CheckBox from 'components/CheckBox';  
+import Input from 'components/Input';  
+import TextArea from 'components/TextArea';  
+import Select from 'components/Select';
+import Button from 'components/Button'
 
 class FormContainer extends Component {  
   constructor(props) {
